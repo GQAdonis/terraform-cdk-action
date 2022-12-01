f08a7af2f19095575051db264f27c4c6dfddeadc
Travis James <travis@tribehealthsolutions.com>
2022-12-01 07:42:14 -0600

    use dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import * as core from "@actions/core";
+import * as dotenv from "dotenv";
 import { run } from "./action";
+
+dotenv.config();
+console.log(process.env);
 
 run().catch((error) => {
   core.setFailed(error.message);
